497934b83a547a348a4425eaadbb8f42a6e0364e
e1b20032 <e1b20032@oit.ac.jp>
2022-10-11 14:11:40 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/z1688/group/lec03/Lec03ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1688.group.lec03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
